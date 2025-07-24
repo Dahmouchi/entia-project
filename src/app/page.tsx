085afd9ef71a053bc1ce737854b6e0007833c0b0
Elhassan_dahmouchi <hassandahmouchi0@gmail.com>
2025-07-24 17:50:29 +0100

    asdffff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -325,10 +325,10 @@ const ModernFeatureCards = ({ features }: any) => {
 const ExampleSection1 = () => {
   return (
     <div className="max-h-screen  w-full" id="courses">
-      <Header />
+      
       <div className="  lg:px-14 px-4 ">
         <div
-          className="rounded-2xl  pt-28 lg:pt-10 flex flex-col lg:flex-row lg:items-center lg:justify-center  h-[80vh] w-full" // 112px = py-14 (3.5rem) * 2
+          className="rounded-2xl relative flex flex-col lg:flex-row lg:items-center lg:justify-center  lg:h-[90vh] h-[80vh] w-full" // 112px = py-14 (3.5rem) * 2
           style={{
             backgroundImage: `url("/Board.png")`,
             backgroundSize: "cover",
@@ -342,7 +342,7 @@ const ExampleSection1 = () => {
             transition={{ duration: 0.8 }}
             className="flex flex-col lg:gap-4 lg:w-1/2 lg:justify-center justify-end  lg:p-14 p-4"
           >
-            <h1 className="text-midnight_text text-xl sm:text-5xl font-semibold  text-white">
+            <h1 className="text-midnight_text text-2xl sm:text-5xl font-semibold mt-30 text-white">
               L&apos;école de demain,{" "}
               <span className="text-blue-500">Aujourd&apos;hui</span>, dans ta
               poche.
@@ -399,7 +399,16 @@ const ExampleSection1 = () => {
               </div>
             </div>
           </motion.div>
-          <motion.div className=" flex justify-center place-items-end  items-end">
+          <motion.div className="  justify-center place-items-end  items-end hidden lg:flex">
+            <Image
+              src="/student5.png"
+              alt="nothing"
+              className="lg:w-4/5 w-2/3 h-auto"
+              width={800}
+              height={805}
+            />
+          </motion.div>
+            <motion.div className=" absolute bottom-0 lg:hidden w-full flex items-center justify-center">
             <Image
               src="/student5.png"
               alt="nothing"
@@ -442,19 +451,17 @@ const ExampleSection2 = () => {
   ];
   return (
     <div
-      className="relative w-full min-h-[100dvh] lg:px-14 px-4 pt-24 pb-14"
+      className="relative w-full h-[100dvh] "
       id="courses"
       style={{
         backgroundImage: "url('/Board.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
-        backgroundAttachment: "fixed",
       }}
     >
       {/* Extra padding for mobile browser UI */}
-      <div className="h-14 md:h-0 w-full fixed top-0 left-0 bg-transparent pointer-events-none" />
 
-      <div className="max-w-6xl mx-auto space-y-4 h-full">
+      <div className="max-w-6xl mx-auto space-y-4 h-full flex flex-col itce justify-center">
         <motion.div
           initial={{ opacity: 0, y: 30 }}
           whileInView={{ opacity: 1, y: 0 }}
@@ -462,7 +469,7 @@ const ExampleSection2 = () => {
           viewport={{ once: true, margin: "100px" }}
           className="text-center"
         >
-          <h2 className="text-3xl md:text-5xl font-bold text-white px-2">
+          <h2 className="text-3xl md:text-5xl font-bold text-white px-2 mt-28">
             Pourquoi choisir <span className="text-indigo-400">Scoolia.ma</span>{" "}
             ?
           </h2>
@@ -513,6 +520,7 @@ const exampleSections = [
 const FullPageScrollExample = () => {
   return (
     <div>
+      <Header />
       <FullPageScrollSystem sections={exampleSections} />
     </div>
   );

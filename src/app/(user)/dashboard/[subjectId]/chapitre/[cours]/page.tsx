9371cd40db8649eeb1e7a1a9a35a8a21db537cce
Elhassan_dahmouchi <hassandahmouchi0@gmail.com>
2025-08-22 13:37:11 +0100

    aa

--- src/app/(user)/dashboard/[subjectId]/chapitre/[cours]/page.tsx
+++ src/app/(user)/dashboard/[subjectId]/chapitre/[cours]/page.tsx
@@ -10,48 +10,13 @@ import ButtonComplete from "@/app/(user)/_components/buttonComplete";
 import { redirect } from "next/navigation";
 
 const CoursePage = async ({ params }: any) => {
-  const course = await getCoursByHandle(params.cours);
   
-  const user = await getStudentById();
-  if (!user) {
-      return redirect("/");
-    }
-  if (!course?.success) {
-    return (
-      <div className="w-full aspect-video bg-gray-100 rounded-lg flex items-center justify-center">
-        <p className="text-gray-500">Sélectionnez un cours pour commencer</p>
-      </div>
-    );
-  }
 
   return (
     <div className="w-full bg-white rounded-lg p-2">
       {/* Lecteur vidéo */}
       <div className="relative w-full aspect-video bg-black rounded-lg overflow-hidden">
-        {course?.data?.videoUrl ? (
-          <div>
-           
-          </div>
-        ) : (
-          <div className="w-full h-full flex items-center justify-center bg-gray-800">
-            <div className="text-center text-white">
-              <Play className="w-16 h-16 mx-auto mb-4 opacity-50" />
-              <p className="text-lg">Vidéo non disponible</p>
-            </div>
-          </div>
-        )}
-      </div>
-
-      {/* Informations du cours */}
-      <div className="mt-4 w-full">
-        <div className="mt-4 flex lg:items-center gap-2 lg:justify-between w-full flex-col lg:flex-row py-3 px-2">
-          <h1 className="text-2xl font-bold text-gray-900 ">
-            {course?.data?.title}
-          </h1>
-          <ButtonComplete userId={user.id} course={course.data}/>
-        </div>
-
-        <CourseContent course={course.data} userId={user.id}/>
+       
       </div>
     </div>
   );
